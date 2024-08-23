757605ba50dd3eebcc4476007631ddbc6ea304a6
vazvdr <azevedo.vanderson@hotmail.com>
2024-08-23 11:28:58 -0300

    Compartilhando os arquivos do projeto

---
+++ apps/mobile/src/data/hooks/useAPI.ts
@@ -0,0 +1,23 @@
+import { useCallback } from 'react'
+
+const URL_BASE = 'http://localhost:3001' // process.env.URL_BASE
+
+export default function useAPI() {
+    const httpGet = useCallback(async function (uri: string): Promise<any> {
+        const res = await fetch(`${URL_BASE}/${uri}`)
+        const data = await res.json()
+        return data
+    }, [])
+
+    const httpPost = useCallback(async function (uri: string, body: any): Promise<any> {
+        await fetch(`${URL_BASE}/${uri}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        })
+    }, [])
+
+    return { httpGet, httpPost }
+}
